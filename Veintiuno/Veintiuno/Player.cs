3e3d22f7f1e5de222c3d407ce54d6ee39033fdaa
Dennis Umaña <dennis2607@hotmail.com>
2020-07-10 12:16:44 -0600

    clase player Dennis Umana

--- Veintiuno/Veintiuno/Player.cs
+++ Veintiuno/Veintiuno/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace Veintiuno {
 
 
@@ -10,9 +11,35 @@ namespace Veintiuno {
      */
     class Player {
 
-        public Player() {
+
+
+        public string namePlayer { get; set; }
+
+        public int score { get; set; }
+
+        public int numberPlayer { get; set; }
+
+        public int home { get; set; }
+
+        public Player(string namePlayer, int score, int numberPlayer, int home)
+        {
+            this.namePlayer = namePlayer;
+            this.score = score;
+            this.numberPlayer = numberPlayer;
+            this.home = home;
 
         }
+
+        /*public static int sumadeCarta(Carta carta)
+        {
+
+
+            return;
+        }*/ 
+
+
+
+
 
     }
 }
